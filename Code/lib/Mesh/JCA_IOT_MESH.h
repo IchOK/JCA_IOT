025ea71fae7ff62b5472254d040570cfbb22b02c
Jochen Cabrera <jochen.matz@googlemail.com>
2020-06-12 12:45:54 +0200

    Add ClientFunctions
    Send / Recv Functions
    Update missing

---
+++ Code/lib/Mesh/JCA_IOT_MESH.h
@@ -0,0 +1,35 @@
+/**********************************************
+ * Definitionen:
+ * Info: 	Definiert allgemeine Konstanten für
+ *        das interne Mesh-Protokoll
+ * Version:
+ * 	V1.0.0	Erstellt	12.06.2020	JCA
+ **********************************************/
+
+#ifndef _JCA_IOT_MESH_H
+#define _JCA_IOT_MESH_H
+
+#define JCA_IOT_MESH_SUBSCRIBE 10
+#define JCA_IOT_MESH_SUBSCRIBE_ACK 11
+#define JCA_IOT_MESH_SUBSCRIBE_PUBLISH 12
+#define JCA_IOT_MESH_SUBSCRIBE_FAIL 19
+#define JCA_IOT_MESH_PUSH 20
+#define JCA_IOT_MESH_DATA_REQUEST 30
+#define JCA_IOT_MESH_DATA_REPLY 31
+#define JCA_IOT_MESH_GETCONF_REQUEST 40
+#define JCA_IOT_MESH_GETCONF_REPLY 41
+#define JCA_IOT_MESH_GETCONF_DATAREQUEST 42
+#define JCA_IOT_MESH_GETCONF_DATA 43
+#define JCA_IOT_MESH_SETCONF_ANNOUNCE 50
+#define JCA_IOT_MESH_SETCONF_DATAREQUEST 51
+#define JCA_IOT_MESH_SETCONF_DATA 52
+#define JCA_IOT_MESH_SETCONF_DONE 53
+#define JCA_IOT_MESH_SRV_PUBLISH 90
+#define JCA_IOT_MESH_SRV_REQUEST 91
+#define JCA_IOT_MESH_SRV_ARCHIVDATA 92
+#define JCA_IOT_MESH_SRV_ALARM 93
+#define JCA_IOT_MESH_SRV_FAILLOG 99
+#define JCA_IOT_MESH_ALARMPRIO_Alarm = 1
+#define JCA_IOT_MESH_ALARMPRIO_WARNING = 2
+#define JCA_IOT_MESH_ALARMPRIO_INFO = 3
+#endif
---
+++ Code/lib/Mesh/JCA_IOT_MESH_Client.h
@@ -0,0 +1,258 @@
+/**********************************************
+ * Class:	JCA_IOT_MESH_Client
+ * Info: 	Die Klasse handelt alle Funktionen rund
+ *        um die Server-Telegramme (Fehler-Logs,
+ *        Archiv-Daten, Alarme, ...)
+ * Version:
+ * 	V1.0.0	Erstellt	12.06.2020	JCA
+ *		-recvSrvPublish
+ *    -sendSrvRequest
+ *    -sendArchiData
+ *    -sendAlarm
+ *    -sendError
+ *    -update
+ **********************************************/
+
+#ifndef _JCA_IOT_MESH_CLIENT_H
+#define _JCA_IOT_MESH_CLIENT_H
+
+//Include extrenal
+#include <map>
+#include <vector>
+#include <ArduinoJson.h>
+
+#include "JCA_IOT_define.h"
+#include "JCA_IOT_MESH.h"
+
+namespace JCA{ namespace IOT{ namespace MESH{
+  enum clientState{
+    init,
+    requesting,
+    online
+  };
+  typedef struct serverState {
+    uint32_t id;
+    uint32_t wd;
+  }serverState;
+  typedef struct serverPublish {
+    uint32_t timestamp;
+    bool logging;
+    bool archvi;
+    bool alarming;
+  }serverPublish;
+  typedef struct archivData {
+    uint32_t timestamp;
+    uint32_t elementIndex;
+    uint32_t datapointIndex;
+    uint16_t dataType;
+    uint16_t type;
+    int32_t  valueInt;
+    float    valueFloat;
+    bool     valueBool;
+  }archivData;
+  typedef struct alarm {
+    uint32_t timestamp;
+    uint16_t prio;
+    String   text;
+  }alarm;
+  typedef struct error{
+    uint16_t type;
+    JsonObject data;
+  }
+  
+  class cClient{
+    public:
+      /***************************************
+       * Methode: cClient()
+       * Info:  Der Konstruktor initialisiert Server-Vektoren
+       *        und Status
+       ***************************************/
+      cClient(){
+        State = init;
+      }
+    
+      /***************************************
+       * Methode: sendSrvRequest(JsonObject Data)
+       * Info:  Funktion zum empfangen und verarbeiten eines
+       *        Server-Publish telegramm.
+       *        Status Updaten
+       *        Server-Listen aktuallisieren
+       ***************************************/
+      void recvSrvPublish(JsonObject Data){
+        bool notFound;
+        uint32_t id;
+        
+        // Uhrzeit stellen
+        
+        // Zu Logging-Server-Liste hinzufügen oder watchdog zurück setzen
+        if (Data.msg["logging"]){
+          notFound = true;
+          id = Data.from;
+          for (int srv = 0; srv < LogServers.size(); srv++){
+            if (LogServers[srv]->id == id){
+              // Watchdog zurücksetzen
+              LogServers[srv]->wd = 0;
+              notFound = false;
+              break;
+            }
+          }
+          if (notFound){
+            LogServers.push_back(serverState());
+            LogServers.back()->id = id;
+            LogServers.back()->wd = 0;
+          }
+        }
+        
+        // Zu Alarming-Server-Liste hinzufügen oder watchdog zurück setzen
+        if (Data.msg["alarming"]){
+          notFound = true;
+          id = Data.from;
+          for (int srv = 0; srv < AlarmServers.size(); srv++){
+            if (AlarmServers[srv]->id == id){
+              // Watchdog zurücksetzen
+              AlarmServers[srv]->wd = 0;
+              notFound = false;
+              break;
+            }
+          }
+          if (notFound){
+            AlarmServers.push_back(serverState());
+            AlarmServers.back()->id = id;
+            AlarmServers.back()->wd = 0;
+          }
+        }
+        
+        // Zu Archiv-Server-Liste hinzufügen oder watchdog zurück setzen
+        if (Data.msg["archiv"]){
+          notFound = true;
+          id = Data.from;
+          for (int srv = 0; srv < ArchivServers.size(); srv++){
+            if (ArchivServers[srv]->id == id){
+              // Watchdog zurücksetzen
+              ArchivServers[srv]->wd = 0;
+              notFound = false;
+              break;
+            }
+          }
+          if (notFound){
+            ArchivServers.push_back(serverState());
+            ArchivServers.back()->id = id;
+            ArchivServers.back()->wd = 0;
+          }
+        }
+      }
+      
+      /***************************************
+       * Methode: sendSrvRequest(JsonObject MeshOut)
+       * Info:  Funktion zur Erstellung eines Server-Request
+       *        Fügt dem MeshOut-Buffer eine Broadcast-Nachricht hinzu
+       ***************************************/
+      void sendSrvRequest(JsonObject &MeshOut){
+        JsonArray Broadcasts;
+        JsonObject Msg;
+        // Falls noch kein Broadcast Eintrag existiert diesen erstellen
+        if (MeshOut->containsKey("broadcast")){
+          Broascasts = MeshOut["broadcast"];
+        }else{
+          Broascasts = MeshOut->createNestedArray("broadcast");
+        }
+        Msg = Broadcasts.createNestedObject();
+        Msg["msgId"] = JCA_IOT_MESH_SRV_REQUEST;
+      }
+      
+      /***************************************
+       * Methode: sendArchivData(JsonObject MeshOut, archivData Data)
+       * Info:  Funktion zur Erstellung eines Archiv Telegramms
+       *        Fügt dem MeshOut-Buffer eine Archiv-Nachricht für
+       *        jeden Server hinzu.
+       ***************************************/
+      void sendArchivData(JsonObject &MeshOut, archivData Data){
+        JsonArray Server;
+        JsonObject Msg;
+        // Alle erfallsten Server durchlaufen
+        for (int srv = 0; srv < ArchivServers.size(); srv++){
+          // Falls noch kein Eintrag für die NodeId existiert, diese erstellen
+          if (MeshOut->containsKey(ArchivServers[srv]->id)){
+            Server = MeshOut[ArchivServers[srv]->id];
+          }else{
+            Server = MeshOut->createNestedArray(ArchivServers[srv]->id);
+          }
+          Msg = Server.createNestedObject();
+          Msg["msgId"] = JCA_IOT_MESH_SRV_ARCHIVDATA;
+          Msg["time"] = Data.timestamp;
+          Msg["element"] = Data.elementIndex;
+          Msg["data"] = Data.datapointIndex;
+          Msg["dataType"] = Data.dataType;
+          switch(Data.dataType){
+            case JCA_IOT_ELEMENT_DATA_BOOL:
+              Msg["value"] = Data.valueBool;
+              break;
+            case JCA_IOT_ELEMENT_DATA_INT:
+              Msg["value"] = Data.valueInt;
+              break;
+            case JCA_IOT_ELEMENT_DATA_FLOAT:
+              Msg["value"] = Data.valueFloat;
+              break;
+          }
+        }
+      }
+      
+      /***************************************
+       * Methode: sendAlarm(JsonObject MeshOut, alarm Data)
+       * Info:  Funktion zur Erstellung eines Alarm Telegramms
+       *        Fügt dem MeshOut-Buffer eine Alarm-Nachricht für
+       *        jeden Server hinzu.
+       ***************************************/
+      void sendAlarm(JsonObject &MeshOut, alarm Data){
+        JsonArray Server;
+        JsonObject Msg;
+        // Alle erfallsten Server durchlaufen
+        for (int srv = 0; srv < AlarmServers.size(); srv++){
+          // Falls noch kein Eintrag für die NodeId existiert, diese erstellen
+          if (MeshOut->containsKey(AlarmServers[srv]->id)){
+            Server = MeshOut[AlarmServers[srv]->id];
+          }else{
+            Server = MeshOut->createNestedArray(AlarmServers[srv]->id);
+          }
+          Msg = Server.createNestedObject();
+          Msg["msgId"] = JCA_IOT_MESH_SRV_ALARM;
+          Msg["time"] = Data.timestamp;
+          Msg["prio"] = Data.prio;
+          Msg["text"] = Data.text;
+        }
+      }
+      
+      /***************************************
+       * Methode: sendError(JsonObject MeshOut, error Data)
+       * Info:  Funktion zur Erstellung eines Archiv Telegramms
+       *        Fügt dem MeshOut-Buffer eine Archiv-Nachricht für
+       *        jeden Server hinzu.
+       ***************************************/
+      void sendError(JsonObject &MeshOut, error Data){
+        JsonArray Server;
+        JsonObject Msg;
+        // Alle erfallsten Server durchlaufen
+        for (int srv = 0; srv < LogServers.size(); srv++){
+          // Falls noch kein Eintrag für die NodeId existiert, diese erstellen
+          if (MeshOut->containsKey(LogServers[srv]->id)){
+            Server = MeshOut[LogServers[srv]->id];
+          }else{
+            Server = MeshOut->createNestedArray(LogServers[srv]->id);
+          }
+          Msg = Server.createNestedObject();
+          Msg["msgId"] = JCA_IOT_MESH_SRV_FAILLOG;
+          Msg["type"] = Data.type;
+          Msg["data"] = Data.data;
+          }
+        }
+      }
+      
+      
+    private:
+      clientState State;
+      std::vector<serverState> LogServers;
+      std::vector<serverState> AlarmServers;
+      std::vector<serverState> ArchivServers;
+  };
+}}}
+#endif
